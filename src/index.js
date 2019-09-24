6b1569c042bba36441291f7268c16dcae3b3a92d
SunshineFett <sunshineFett@gmail.com>
2019-09-24 05:12:21 -0400

    comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const logo = document.querySelector("#logo");
-logo.innerText = "YO!";
+
+//compared to
+//const logo = document.getElementById("logo");
+
+//overwrites the value inside the HTML
+//logo.innerText = "YO!";
 
 const heading = document.createElement("h1");
 heading.innerHTML = "<span>Hello</span>" + document.URL;
